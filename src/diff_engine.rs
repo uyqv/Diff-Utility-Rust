75ca102543d6a97d0812d3994290a2a8fa49cbda
andre <andrew.shatsky@gmail.com>
2024-04-11 22:14:56 -0400

    Improved diff_engine.rs

--- src/diff_engine.rs
+++ src/diff_engine.rs
@@ -1,50 +1,33 @@
-use rayon::prelude::*; // enables data-parallel operations
+// use rayon::prelude::*;
+use std::str::from_utf8;
+
+#[derive(Debug, PartialEq)]
+pub struct Difference<'a> {
+    pub from: &'a str,
+    pub to: &'a str,
+}
 
 pub struct DiffEngine {}
 
-#[derive(Debug, PartialEq)] // automatically implements PartialEq for Difference
-pub struct Difference {
-    pub from: String,
-    pub to: String,
-}
-
-// constructor for DiffEngine
-// RETURNS: an instance of DiffEngine
 impl DiffEngine {
     pub fn new() -> DiffEngine {
         DiffEngine {}
     }
 
-    // compares two chunks and returns a list of differences
-    // PARAMETERS: two chunks of data
-    // RETURNS: a list of Differences 
-    pub fn compare_chunks(&self, chunk1: &[u8], chunk2: &[u8]) -> Vec<Difference> {
-        // convert byte slices to UTF-8 strings
-        let content1 = std::str::from_utf8(chunk1).unwrap_or_default();
-        let content2 = std::str::from_utf8(chunk2).unwrap_or_default();
-
-        // split the strings into lines or units for comparison
-        let lines1: Vec<&str> = content1.lines().collect();
-        let lines2: Vec<&str> = content2.lines().collect();
-
-        // find the minimum length to avoid index out of bounds
-        let min_length = lines1.len().min(lines2.len());
-
-        // compare each line in parallel
-        lines1
-            .into_par_iter() 
-            .zip(lines2.into_par_iter()) // pairs elements together, preparing for parallel processing
-            .enumerate() // tranforms the iterator of pairs into an iterator of tuples
+    pub fn compare_chunks<'a>(&self, chunk1: &'a [u8], chunk2: &'a [u8]) -> Vec<Difference<'a>> {
+        let content1 = from_utf8(chunk1).unwrap_or_default();
+        let content2 = from_utf8(chunk2).unwrap_or_default();
+        let min_length = content1.len().min(content2.len());
+        content1.lines()
+            .zip(content2.lines())
+            .enumerate()
             .filter_map(|(index, (line1, line2))| {
                 if index < min_length && line1 != line2 {
-                    Some(Difference {
-                        from: line1.to_string(),
-                        to: line2.to_string(),
-                    })
+                    Some(Difference { from: line1, to: line2 })
                 } else {
                     None
                 }
             })
-            .collect() // collects result from filter_map that was flagged by "Some(..)"
+            .collect()
     }
 }
